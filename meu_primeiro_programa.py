6f547cc2bbbd04bc65e93709454514eec1733255
ninjaferoztrindade <ninjaferoztrindade@gmail.com>
2025-08-07 10:10:12 -0300

    meu_primeiro_programa.py

---
+++ meu_primeiro_programa.py
@@ -0,0 +1,37 @@
+a=4
+b=10
+c=5.0
+d=1
+f=5
+print(a==c)
+print(a<b)
+print(d>b)
+print(c!=f)
+print(a==b)
+print(c<d)
+print(b>a)
+print(c>=f)
+print(f>=c)
+print(c<=c)
+print(c<=f)
+print('------x------')
+a=True
+b=False 
+c=True
+print(not b)
+print(a or b)
+print(b and b)
+print('------x------')
+print(a and a)
+print(b and b) 
+print(not c)
+print(not b)
+print(not a)
+print(a and b)
+print(b and c)
+print(a or c)
+print(b or c)
+print(c or a)
+print(c or b)
+print(c or c)
+print(b or b)
