ddf7e2d745f2d6876e12b1e700423ecb4dc90f5c
ninjaferoztrindade <ninjaferoztrindade@gmail.com>
2025-09-02 10:50:11 -0300

    exercicios_automação

---
+++ Automação.py
@@ -0,0 +1,55 @@
+import mouseinfo, pyautogui, time
+
+
+#mouseinfo.mouseInfo()
+pyautogui.moveTo(1165,1068, duration=1)
+
+pyautogui.click()
+pyautogui.moveTo(424,70, duration=1)
+pyautogui.click()
+
+pyautogui.write("iron maiden")
+pyautogui.press("Enter")
+pyautogui.doubleClick()
+ 
+pyautogui.write("Chaves")
+pyautogui.press("Enter")
+ 
+pyautogui.doubleClick()
+
+pyautogui.write("mercado livre")
+pyautogui.press("Enter")
+
+pyautogui.doubleClick()
+
+pyautogui.write("shein")
+pyautogui.press("Enter")
+
+pyautogui.doubleClick()
+
+pyautogui.write("kwai")
+pyautogui.press("Enter")
+
+pyautogui.doubleClick()
+
+pyautogui.write("growth supplements")
+pyautogui.press("Enter")
+
+pyautogui.doubleClick()
+
+pyautogui.write("chapolim")
+pyautogui.press("Enter")
+
+pyautogui.doubleClick()
+
+pyautogui.write("nike")
+pyautogui.press("Enter")
+
+pyautogui.doubleClick()
+
+pyautogui.write("banda de rock mais famosa do mundo")
+pyautogui.press("Enter")
+
+pyautogui.doubleClick()
+pyautogui.write("diferente do mc donolds o Burguer king é feito no fogo de verdade")
+pyautogui.press("Enter")
